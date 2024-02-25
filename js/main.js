37973c0760bfab4d8865e2950e467e01b5aaeffb
udeilton de sousa <deilton_black@hotmail.com>
2024-02-25 12:51:44 -0300

    Teclado|js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const keys = document.querySelectorAll('.key');
+
+const playNote = (note) => {
+    const audio = new Audio(`../notes/${note}.wav`);
+    audio.play();
+}
+
+const handleMouseDown = (key) =>{
+    playNote(key.getAttribute('data-note'));
+
+    if (key.className.includes('black')) {
+        key.classList.add('black--pressed')
+        return;
+    }
+    key.style.background = '#ddd';
+}
+
+const handleMouseUp = (key) =>{
+    if (key.className.includes('black')) {
+        key.classList.remove('black--pressed');
+        return;
+    }
+    key.style.background = 'white';
+}
+
+keys.forEach((key) => {
+    key.addEventListener('mousedown', () => handleMouseDown(key));
+
+    key.addEventListener('mouseup', () => handleMouseUp(key));
+
+});
